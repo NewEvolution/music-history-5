c82607806bb2b7ae2c518831a3dd75f73343b21a
Ryan Tanay <ryantanay@newevolution.org>
2015-08-01 08:54:43 -0500

    Had a revelation, fought with it for an hour, realized I still had no idea what I was doing. Ended up adding a single comment.

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,10 +1,8 @@
 requirejs(["dom-access", "populate-songs", "get-more-songs"], function(dom, populate, more){
-  //populate.getSongs();
   $.ajax().done(function(data) {
     songWriter(populate.giveSongs(), dom.getDomElement());
   });
   $("#more").click(function(e) {
-    //more.getSongs();
     $.ajax().done(function(data) {
       songWriter(more.giveSongs(), dom.getDomElement());
     });
@@ -12,9 +10,9 @@ requirejs(["dom-access", "populate-songs", "get-more-songs"], function(dom, popu
 });
 
 
-function songWriter(dataArr, domObject) {
+function songWriter(dataArr, domEle) {
   for(var i=0; i<dataArr.length; i++) {
-    domObject.before("<section><button class='delete'>Delete</button><h1>" + 
+    domEle.before("<section><button class='delete'>Delete</button><h1>" + 
       dataArr[i].title + "</h1><ul><li>" + dataArr[i].artist + 
       "</li><li><span></span></li><li>" + dataArr[i].album + 
       "</li><li><span></span></li><li>" + dataArr[i].genre + 
@@ -22,6 +20,7 @@ function songWriter(dataArr, domObject) {
   }
 }
 
+// Remove button for song sections
 $(".content").on("click", ".delete", function(e) {
   $(this).parent().remove();
 });
